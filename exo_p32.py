06c72ae79582919f1e0ba17b7bce66a5520bcfca
Jérôme Mainaud <jerome@mainaud.com>
2022-11-20 18:23:27 +0100

    Simplification

--- exo_p32.py
+++ exo_p32.py
@@ -4,15 +4,16 @@ import numpy as np
 simulator = q.Aer.get_backend('statevector_simulator')
 
 def main():
-    baba_y()
+    b_a_ba_y()
     etat_init = etat_initial()
-    print(f"état initial   = [{etat_init[0]:.6f}; {etat_init[1]:.6f}]")
     resultat_h = executer(circuit_h())
-    print(f"resultat H     = [{resultat_h[0]:.6f}; {resultat_h[1]:.6f}]")
     resultat_hx = executer(circuit_hx())
-    print(f"resultat H X   = [{resultat_hx[0]:.6f}; {resultat_hx[1]:.6f}]")
     resultat_hxy = executer(circuit_hxy())
-    print(f"resultat H X Y = [{resultat_hxy[0]:.6f}; {resultat_hxy[1]:.6f}]")
+
+    print_vector('|psy>               ', etat_init)
+    print_vector('|psy> -> H          ', resultat_h)
+    print_vector('|psy> -> H -> X     ', resultat_hx)
+    print_vector('|psy> -> H -> X -> Y', resultat_hxy)
 
 
 def etat_initial():
@@ -41,7 +42,7 @@ def circuit_hxy():
     circuit.y(0)
     return circuit
 
-def baba_y():
+def b_a_ba_y():
     circuit1 = q.QuantumCircuit(1)
     circuit1.append(q.extensions.Initialize([1, 0]), [0])
     circuit1.y(0)
@@ -52,9 +53,11 @@ def baba_y():
     circuit2.y(0)
     r1 = executer(circuit2)
 
-    print(f'Y(|0>) = [{r0[0]:.6f}; {r0[1]:.6f}')
-    print(f'Y(|1>) = [{r1[0]:.6f}; {r1[1]:.6f}')
+    print_vector('Y(|0>)', r0)
+    print_vector('Y(|1>)', r1)
 
+def print_vector(name: str, vector) -> None:
+    print(f'{name} = [{vector[0]:.6f}; {vector[1]:.6f}')
 
 def executer(circuit):
     job = q.execute(circuit, simulator)
